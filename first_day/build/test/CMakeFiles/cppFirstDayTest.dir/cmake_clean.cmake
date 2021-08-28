d610efef67d200f5002c1e942668b8ca824ed70c
SevenZhan <seven.zhan92@gmail.com>
2021-08-28 23:15:25 +0800

    [Cpp Tutorial] first commit

---
+++ first_day/build/test/CMakeFiles/cppFirstDayTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppFirstDayTest.dir/src/main.cpp.o"
+  "cppFirstDayTest"
+  "cppFirstDayTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppFirstDayTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ first_day/test/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+
+int main()
+{
+    std::cout << "the test binary is working\n";
+
+    return 0;
+}
